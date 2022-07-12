7f7b78f9b5c83d5d1648ac6d9b991e94ecadabf1
nontas <tsakoto90@gmail.com>
2022-07-13 01:45:59 +0300

    APIs complete static

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const request = require("request");
 
 const app = express();
 
-app.use(express("public"));
+app.use(express.static("public"));
 
 app.get("/",function(req, res){
   res.sendFile(__dirname + "/signup.html");
