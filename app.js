640db73aa77da24db24a98cb0f567dfe2e0ade45
nontas <tsakoto90@gmail.com>
2022-07-21 12:00:25 +0300

    app.use

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req, res){
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+
+})
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
 });
+
+
+
+// e881d0f6e95ccfb3537f6666455230aa-us14
